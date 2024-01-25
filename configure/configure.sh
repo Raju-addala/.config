432b97afcf558d82dd32aeef94ead15a5fbb414b
Raju Addala <rajuaddala7427@gmail.com>
2024-01-25 19:46:09 +0530

    add plugins and configure conditions

--- configure/configure.sh
+++ configure/configure.sh
@@ -1,18 +1,18 @@
 BASEDIR=$(dirname $0)
 
 # check if CONFIGURE_DIR env exists
-if [[ -z "$CONFIGURE_DIR" ]]
+if [ -z "$CONFIGURE_DIR" ]
 then
     echo 'export CONFIGURE_DIR="'${BASEDIR}'"' >> ~/.mysetup.sh
 fi
 
 file_name="configure.sh"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]
+if [ "$OSTYPE" = "linux-gnu"* ]
 then
         # ...
     sudo apt update
-elif [[ "$OSTYPE" == "darwin"* ]]
+elif [ "$OSTYPE" = "darwin"* ]
 then
     echo "mac"
 fi
