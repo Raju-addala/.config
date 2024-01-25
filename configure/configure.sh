4047497684ac62820be294477520772ffe46b7b8
Raju Addala <anarayanaraju@enphaseenergy.com>
2024-01-25 19:19:29 +0530

    rebase

--- configure/configure.sh
+++ configure/configure.sh
@@ -1,5 +1,20 @@
+BASEDIR=$(dirname $0)
+
+# check if CONFIGURE_DIR env exists
+if [[ -z "$CONFIGURE_DIR" ]]
+then
+    echo 'export CONFIGURE_DIR="'${BASEDIR}'"' >> ~/.mysetup.sh
+fi
+
 file_name="configure.sh"
 
-sudo apt update
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+then
+        # ...
+    sudo apt update
+elif [[ "$OSTYPE" == "darwin"* ]]
+then
+    echo "mac"
+fi
 
-./zsh/{$file_name}
+$BASEDIR/zsh/${file_name}
